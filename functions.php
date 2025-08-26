d7f95247df07e0a2152b35ea606bc4c8ac030a84
minhpham@365villas.com <minhpham@365villas.com>
2025-08-26 15:37:12 +0700

    add checker update

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+require "plugin-update-checker-v5/plugin-update-checker.php";
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://example.com/path/to/details.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'unique-plugin-or-theme-slug'
+);
+
+function divi__child_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
+ 
+//you can add custom functions below this line:
+function wpc_remove_height_cropping($height) {
+	return '9999';
+}
+function wpc_remove_width_cropping($width) {
+	return '9999';
+}
+add_filter( 'et_pb_blog_image_height', 'wpc_remove_height_cropping' );
+add_filter( 'et_pb_blog_image_width', 'wpc_remove_width_cropping' );
+
+function add_json_upload_support($mimes) {
+    $mimes['json'] = 'application/json';
+    return $mimes;
+}
+add_filter('upload_mimes', 'add_json_upload_support');
+
+
